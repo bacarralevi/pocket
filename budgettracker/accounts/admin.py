9798b92f3e9f41b8aa42226d8b1b39142bb46e1c
cidmesa <chrizian06@gmail.com>
2025-04-30 05:03:17 +0800

    Added export csv button on transaction history

--- budgettracker/accounts/admin.py
+++ budgettracker/accounts/admin.py
@@ -1,7 +1,8 @@
 # accounts/admin.py
 
 from django.contrib import admin
-from .models import Transaction, Category  # Import Category
+from .models import Transaction, Category, Budget  # Import Category
 
 admin.site.register(Transaction)
 admin.site.register(Category)  
+admin.site.register(Budget)
